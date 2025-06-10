d0d32e9c349fccf921c2b9cde41a4d208f913044
cuerpo-textual <nico.rivera@alumnos.ucn.cl>
2025-06-09 20:39:02 -0400

    update main.js

--- public/main.js
+++ public/main.js
@@ -5,7 +5,7 @@ let mensajesGlobal = [];
 
 async function cargarMensajes() {
   const res = await fetch(`${BACKEND_URL}/mensajes`);
-  const mensajes = await res.json();
+  mensajesGlobal = await res.json(); // <-- Guarda los mensajes
   renderizarMensajes();
 }
 
@@ -13,19 +13,19 @@ function renderizarMensajes() {
   const contenedor = document.querySelector("#mensajes");
   const orden = document.getElementById("orden").value;
   const busqueda = document.getElementById("busqueda").value.toLowerCase();
-  
-  let mensajesFiltrados = mensajesGlobal.filter(({ nombre, texto})) =>
+
+  let mensajesFiltrados = mensajesGlobal.filter(({ nombre, texto }) =>
     nombre.toLowerCase().includes(busqueda) || texto.toLowerCase().includes(busqueda)
   );
-  
+
   if (orden === "recientes") {
     mensajesFiltrados = mensajesFiltrados.slice().reverse();
   }
 
   contenedor.innerHTML = "";
-  mensajes.Filtrados.forEach(({ d, nombre, texto, timestamp}) => {
+  mensajesFiltrados.forEach(({ id, nombre, texto, timestamp }) => {
     const div = document.createElement("div");
-    div.classname = "mensaje";
+    div.className = "mensaje";
     div.innerHTML = `
       <strong>${nombre}</strong>
       <p><em>${new Date(timestamp).toLocaleString("es-CL", { timeZone: "America/Santiago" })}</em></p>
@@ -38,19 +38,6 @@ function renderizarMensajes() {
 document.getElementById("orden").addEventListener("change", renderizarMensajes);
 document.getElementById("busqueda").addEventListener("input", renderizarMensajes);
 
-
-  mensajes.slice().reverse().forEach(({ id, nombre, texto, timestamp }) => {
-    const div = document.createElement("div");
-    div.className = "mensaje";
-    div.innerHTML = `
-      <strong>${nombre}</strong> 
-      <p><em>${new Date(timestamp).toLocaleString("es-CL", { timeZone: "America/Santiago" })}</em></p>
-      <p>${texto}</p>
-    `;
-    contenedor.appendChild(div);
-  });
-}
-
 document.querySelector("#formulario").addEventListener("submit", async (e) => {
   e.preventDefault();
   const form = e.target;
@@ -58,10 +45,10 @@ document.querySelector("#formulario").addEventListener("submit", async (e) => {
   const texto = form.texto.value;
 
   await fetch(`${BACKEND_URL}/mensajes`, {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ nombre, texto })
-});
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ nombre, texto })
+  });
 
   form.reset();
   cargarMensajes();
