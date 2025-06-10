3913e1be729cc3a35974e79da7f649d4fb2cf6f5
cuerpo-textual <nico.rivera@alumnos.ucn.cl>
2025-06-09 20:20:25 -0400

    search update

--- public/main.js
+++ public/main.js
@@ -1,12 +1,43 @@
 const BACKEND_URL = "https://foro-textual.onrender.com"; // Cámbialo por tu URL real
 const ADMIN_KEY = "n1c0lovezlynx"; // Sólo si tú quieres borrar
 
+let mensajesGlobal = [];
+
 async function cargarMensajes() {
   const res = await fetch(`${BACKEND_URL}/mensajes`);
   const mensajes = await res.json();
+  renderizarMensajes();
+}
 
+function renderizarMensajes() {
   const contenedor = document.querySelector("#mensajes");
+  const orden = document.getElementById("orden").value;
+  const busqueda = document.getElementById("busqueda").value.toLowerCase();
+  
+  let mensajesFiltrados = mensajesGlobal.filter(({ nombre, texto})) =>
+    nombre.toLowerCase().includes(busqueda) || texto.toLowerCase().includes(busqueda)
+  );
+  
+  if (orden === "recientes") {
+    mensajesFiltrados = mensajesFiltrados.slice().reverse();
+  }
+
   contenedor.innerHTML = "";
+  mensajes.Filtrados.forEach(({ d, nombre, texto, timestamp}) => {
+    const div = document.createElement("div");
+    div.classname = "mensaje";
+    div.innerHTML = `
+      <strong>${nombre}</strong>
+      <p><em>${new Date(timestamp).toLocaleString("es-CL", { timeZone: "America/Santiago" })}</em></p>
+      <p>${texto}</p>
+    `;
+    contenedor.appendChild(div);
+  });
+}
+
+document.getElementById("orden").addEventListener("change", renderizarMensajes);
+document.getElementById("busqueda").addEventListener("input", renderizarMensajes);
+
 
   mensajes.slice().reverse().forEach(({ id, nombre, texto, timestamp }) => {
     const div = document.createElement("div");
